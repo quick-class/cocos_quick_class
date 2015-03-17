401ce9c2b63add8da6e7f764ade99f51c7501e77
yydgit <137954671@qq.com>
2015-03-17 22:02:43 +0800

    通过 getButton()方法获取button 在场景中设置属性

--- ui/MyButton.lua
+++ ui/MyButton.lua
@@ -1,37 +1,42 @@
---[[
-local myButton = MyButton.new({normal="img/star.png"},self,function()
---do something
+--[[ 
+	- 带特定动作的按钮 
+    local myButton = MyButton.new({normal = "img/star.png"},self,function()
+        print("listener")
+    end)
+	myButton:getButton()
+			:pos(300,200)
+			:addTo(self)
 end)
 --]]
-
-
 local MyButton = class("MyButton")
 function MyButton:ctor(params,scene,listener)
-    self.button = nil
+    self.listener = listener
+    self.button = cc.ui.UIPushButton.new("img/star.png")
+        --:center()
+        --:addTo(scene)
+end
+function MyButton:getButton()
     local time = 0.1
     local offset = 40
-    self.button = cc.ui.UIPushButton.new(params)
-        :onButtonClicked(function()
-            local spawn1 = cc.Spawn:create({
-                cc.MoveBy:create(time,cc.p(0,-offset)),
-                cc.ScaleTo:create(time,1,0.3),
+    self.button:onButtonClicked(function()
+    local spawn1 = cc.Spawn:create({
+        cc.MoveBy:create(time,cc.p(0,-offset)),
+        cc.ScaleTo:create(time,1,0.3),
+    })
+    
+    transition.execute(self.button,spawn1,{
+        onComplete = function()
+            local spawn2 = cc.Spawn:create({
+                cc.MoveBy:create(time,cc.p(0,offset)),
+                cc.ScaleTo:create(time,1,1)
             })
-
-            transition.execute(self.button,spawn1,{
-                onComplete = function()
-                    local spawn2 = cc.Spawn:create({
-                        cc.MoveBy:create(time,cc.p(0,offset)),
-                        cc.ScaleTo:create(time,1,1)
-                    })
-                    transition.execute(self.button,spawn2,{
-                        easing = "backOut",
-                        onComplete = listener
-                    })    
-                end
-            })
-        end)
-        :center()
-        :addTo(scene)
+            transition.execute(self.button,spawn2,{
+                easing = "backOut",
+                onComplete = self.listener
+            })    
+        end})
+    end)
+    
     return self.button
 end
 return MyButton
